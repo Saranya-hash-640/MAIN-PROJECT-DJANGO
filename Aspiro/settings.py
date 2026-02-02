4a8588b0a33c9cbcdf6ad449fd3a9c6a269a4fec
Saranya N M <saranyanm2012@gmail.com>
2026-02-02 13:26:15 +0530

    second commit

--- Aspiro/settings.py
+++ Aspiro/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY ="django-insecure-XXXX"
 
 DEBUG = False
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [saranyanm.pythonanywhere.com]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
