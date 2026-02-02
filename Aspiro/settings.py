92e409e4521872dc493f94cce5cc74d0ef431b63
Saranya N M <saranyanm2012@gmail.com>
2026-02-02 14:19:20 +0530

    second commit

--- Aspiro/settings.py
+++ Aspiro/settings.py
@@ -10,8 +10,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # -------------------------
 # SECURITY
 # -------------------------
-SECRET_KEY = config('DJANGO_SECRET_KEY', default='django-insecure-XXXX')
+from decouple import config
+
+SECRET_KEY = config('DJANGO_SECRET_KEY')
+RAZORPAY_KEY_ID = config('RAZORPAY_KEY_ID')
+RAZORPAY_KEY_SECRET = config('RAZORPAY_KEY_SECRET')
 DEBUG = config('DEBUG', default=False, cast=bool)
+
 ALLOWED_HOSTS = ['saranyanm.pythonanywhere.com']
 
 # -------------------------
